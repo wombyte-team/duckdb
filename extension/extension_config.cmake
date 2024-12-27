a9fc9dddff659e64f73d5a9da424439bf90afefa
kononencheg <kononencheg@gmail.com>
2024-12-27 08:59:48 +0200

    Reset default extensions

--- extension/extension_config.cmake
+++ extension/extension_config.cmake
@@ -9,9 +9,9 @@
 
 # Parquet is loaded by default on every build as its a essential part of DuckDB
 duckdb_extension_load(parquet)
-duckdb_extension_load(json)
-duckdb_extension_load(icu)
-duckdb_extension_load(httpfs)
+#duckdb_extension_load(json)
+#duckdb_extension_load(icu)
+#duckdb_extension_load(httpfs)
 
 # The Linux allocator has issues so we use jemalloc, but only on x86 because page sizes are fixed at 4KB.
 # Configuring jemalloc properly for 32bit is a hassle, and not worth it so we only enable on 64bit
